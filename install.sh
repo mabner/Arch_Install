adb7f3c581e3af3ad95d0440fe4d58dd45e13667
Marcos Leite <30819867+mabner@users.noreply.github.com>
2024-02-16 01:43:58 -0300

    removes wifi setup as nmcli isn't working inside live boot chroot and adds pause for debugging

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+function pause(){
+  read -t 6
+}
+
 # Input parameters
 echo "Enter the patch to the EFI partition: "
 read EFI
@@ -31,14 +35,17 @@ fi
 echo -e "\nFormating partitions\n"
 mkfs.ext4 "${ROOT}" -L "Arch"
 mkswap "${SWAP}" -L "Swap"
+pause
 
 # Mounting targets
 mount "${ROOT}" /mnt
 mount --mkdir "${EFI}" /mnt/boot
 swapon "${SWAP}"
+pause
 
 # Mirror update
 reflector --country Brazil --latest 10 --protocol http,https --sort rate --save /etc/pacman.d/mirrorlist
+pause
 
 # Pacstrap the base and base-devel with usual dependencies
 if [[ $WIFI_OPT == '1' ]]
@@ -47,67 +54,80 @@ then
 else
   pacstrap -K /mnt base base-devel linux linux-firmware nano git man-db texinfo networkmanager bash-completion grub os-prober efibootmgr
 fi
+pause
 
 # fstab
 genfstab -U /mnt >> /mnt/etc/fstab
-
-# Stops iwctl in order to run nmcli inside chroot
-systemctl stop iwd.service
+pause
 
 # Script part to run inside chroot
 #######################################################################
 cat <<CHROOT > /mnt/chroot.sh
 
+function pause(){
+  read -t 6
+}
+
 # Sets the timezone
 ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
+pause
 
 # Generates /etc/adjtime
 hwclock --systohc
+pause
 
 # Setting the locale
 echo "en_GB.UTF-8 UTF-8" > /ect/locale.gen
 echo "pt_BR.UTF-8 UTF-8" >> /ect/locale.gen
+pause
+
+cat /etc/locale.gen
+pause
+
 locale-gen
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
+pause
+
+echo "LANG=en_GB.UTF-8" > /etc/locale.conf
 echo "KEYMAP=uk" > /etc/vconsole.conf
+pause
 
 # Hostname
 echo "$HOST" > /etc/hostname
-cat <<EOF > /etc/hosts
-127.0.0.1	localhost
-::1			localhost
-127.0.1.1	"$HOST".localdomain	"$HOST"
-EOF
 
 # Enable Network Manager amd set-up Wifi
-systemctl enable NetworkManager.service
-systemctl start NetworkManager.service
-if [[ $WIFI_OPT == '1' ]]
-then
-  nmcli device wifi connect "$SSID" password "$WIFI_PASS"
-else
-  echo "No Wifi to set-up"
-fi
+#systemctl enable NetworkManager.service
+#systemctl start NetworkManager.service
+#if [[ $WIFI_OPT == '1' ]]
+#then
+#  nmcli device wifi connect "$SSID" password #"$WIFI_PASS"
+#else
+#  echo "No Wifi to set-up"
+#fi
 
 # Root password
 echo root:$ROOT_PASSWORD | chpasswd
+pause
 
 # Adding the normal user with sudo abilities
 useradd -m -G wheel,storage,power,audio -s /bin/bash $USERNAME
 echo $USERNAME:$USER_PASSWORD | chpasswd
+pause
 
 # Enable sudo for wheel users
 sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
+pause
 
 # GRUB install
 ## Install GRUB
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Arch
+pause
 
 ## Enables GRUB os-prober
 sed -i 's/^#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/' /etc/default/grub
 
 ## Generates the GRUB config
 grub-mkconfig -o /boot/grub/grub.cfg
+pause
 
 CHROOT
 #######################################################################
