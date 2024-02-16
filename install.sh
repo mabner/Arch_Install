a72923ca14a8387c528c3cdd32ffefa701dbc23f
Marcos Leite <m.abner@live.co.uk>
2024-02-15 22:47:57 -0300

    sets the filesystem, root, swap and mount

--- install.sh
+++ install.sh
@@ -16,3 +16,16 @@ read USER
 echo "Enter the user password: "
 read USER_PASSWORD
 
+# Setting the filesystem
+echo -e "\nFormating partitions\n"
+
+mkfs.ext4 "${ROOT}" -L "Arch"
+mkswap "${SWAP}" -L "Swap"
+swapon "${SWAP}"
+
+# Mounting targets
+mount "${ROOT} /mnt"
+mkdir /mnt/boot
+mount "${EFI} /mnt/boot"
+
+
