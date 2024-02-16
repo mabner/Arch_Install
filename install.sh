cb19f7a7d998a6634a5edfe6da25f2130856c034
Marcos Leite <m.abner@live.co.uk>
2024-02-15 22:45:41 -0300

    adds input parameters

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Input parameters
+echo "Enter the patch to the EFI partition: "
+read EFI
+echo "Enter the path to the ROOT partition: "
+read ROOT
+echo "Enter the path to the SWAP partition: "
+read SWAP
+echo "Enter the ROOT password"
+read ROOT_PASSWORD
+echo "Enter the HOSTNAME: "
+read HOSTNAME
+echo "Enter the username: "
+read USER
+echo "Enter the user password: "
+read USER_PASSWORD
+
